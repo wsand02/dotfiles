d22c214e7e855c13ff60892902f8140d99a61ebd
William Sandbrink <29465936+wsand02@users.noreply.github.com>
2026-03-29 16:04:15 +0200

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+dotfilesdir="${HOME}/dotfiles" 
+cd ${dotfilesdir} || exit
+configdir="${dotfilesdir}/.config"
+kittydir="${configdir}/kitty"
+lfdir="${configdir}/lf"
+
+echo "${dotfilesdir}/.zshrc"
+ln -s -f "${dotfilesdir}/.zshrc" "${HOME}/.zshrc"
+ln -s -f "${dotfilesdir}/.p10k.zsh" "${HOME}/.p10k.zsh"
+
